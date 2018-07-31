3b55ba482115153f615f0f2e898bb39f2ae54eff
Dim <dimitri.kiavue@gmail.com>
2018-07-31 12:52:33 +0200

    New_user fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 #=============================STATICS===========================
   get   '/', to: 'static_pages#home'
 #==============================USERS============================
-  get   '/new_user',  to: 'user#new'
+  get   '/new_user',  to: 'user#new_user'
   post  '/new_user',  to: 'user#create'
 end
