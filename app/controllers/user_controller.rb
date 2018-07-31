b014811ec333c55de1f75c823bc68201626e83ba
Dim <dimitri.kiavue@gmail.com>
2018-07-31 14:19:16 +0200

    User attribute Mail fixed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
     @user_name= params["username"]
     @user_mail= params["email"]
     @user_bio= params["bio"]
-    user = User.create(username: @user_name, mail: @user_mail, bio: @user_bio)
+    user = User.create(username: @user_name, email: @user_mail, bio: @user_bio)
     puts "User créé!"
     redirect_to "/new_user"
   end
