7fe6c7de0b38543a8feae809ce6ccb3bf717bd87
Dim <dimitri.kiavue@gmail.com>
2018-07-31 18:37:28 +0200

    Forms types validated

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,18 +2,40 @@ class UserController < ApplicationController
   def new_user
 
   end
-
+#==============================FORM_FOR_METHOD=============================
+=begin
   def create
     @user= User.new
-    @user.username= params[:username]
-    @user.email= params[:email]
-    @user.bio= params[:bio]
+    @user.username= params["user"][:username]
+    @user.email= params["user"][:email]
+    @user.bio= params["user"][:bio]
     @user.save
     puts "User créé!"
     redirect_to "/new_user"
   end
+=end
+#==============================FORM_TAG_METHOD=============================
+=begin
+   def create
+     @user= User.new
+     @user.username= params[:username]
+     @user.email= params[:email]
+     @user.bio= params[:bio]
+     @user.save
+     puts "User créé!"
+     redirect_to "/new_user"
+   end
+=end
+#==============================FORM_HTML_METHOD=============================
 
-  def create_form_for
-    @user= User.new    
+   def create
+      @user_name= params["username"]
+      @user_email= params["email"]
+      @user_bio= params["bio"]
+      user = User.create(username: @user_name, email: @user_email, bio: @user_bio)
+      puts "User créé!"
+      redirect_to "/new_user"
   end
+
+
 end
